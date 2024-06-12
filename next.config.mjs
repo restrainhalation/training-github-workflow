9b4181403123c6f2eaf00097f93dab853380563b
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-12 21:39:52 +0900

    🛣️ chore: adjust prefix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+// 環境差を埋めるためのパスのプレフィックス
+const prefix = process.env.NODE_ENV === 'production' ? '/training-github-workflow' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: prefix,
+  basePath: prefix,
 };
 
 export default nextConfig;
