e296a1768e4ff3b41175093df3a1b6b226e5ef0e
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-12 22:21:08 +0900

    🧘 chore: merge github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-// 環境差を埋めるためのパスのプレフィックス
-const prefix = process.env.NODE_ENV === 'production' ? '/training-github-workflow' : ''
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: prefix,
-  basePath: prefix,
-};
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import nextConfig from '../../next.config.mjs'
+const BASE_PATH = nextConfig.basePath ? nextConfig.basePath : ''
 
 export default function Home() {
   // 環境差を埋めるためのパスのプレフィックス
-  const prefix = process.env.NODE_ENV === 'production' ? '/training-github-workflow' : ''
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -19,7 +21,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src={`${prefix}/vercel.svg`}
+              src={`${BASE_PATH}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -33,7 +35,7 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src={`${prefix}/next.svg`}
+          src={`${BASE_PATH}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
