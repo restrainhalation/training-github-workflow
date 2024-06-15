590aad8b476fe7af5d5f0a28b8493d74e41a0659
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-15 13:20:10 +0900

    🎨 chore: fix src attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,20 @@ import '@mantine/core/styles.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
+import nextConfig from '../next.config.mjs';
 
 export const metadata = {
   title: 'Mantine Next.js template',
   description: 'I am using Mantine with Next.js!',
 };
+const BASE_PATH = nextConfig.basePath ? nextConfig.basePath : '';
 
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href={`${BASE_PATH}/favicon.svg`} />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
