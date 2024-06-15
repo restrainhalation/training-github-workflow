0db35860ec8d9fa4c7cbf73fdf4593bf881b11d5
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-15 13:07:11 +0900

    🔨 chore: remove step of actions/configure-pages@v5

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
+import { name } from './package.json'
+
+// 環境差を埋めるためのパスのプレフィックス
+const prefix = process.env.NODE_ENV === 'production' ? `/${name}` : ''
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
 export default withBundleAnalyzer({
+  images: { unoptimized: true },
+  assetPrefix: prefix,
+  basePath: prefix,
+  output: 'export',
+
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
